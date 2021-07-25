762c5ffe3c805389f91cb603cabb4c1f9e5d8c31
Lukáš Hejnal <kejhy93@gmail.com>
2021-07-25 14:38:52 +0200

    Use Jedis

--- src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
+++ src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
@@ -1,16 +1,22 @@
 package org.hejnaluk.gamenewscrawler.configuration;
 
+import org.hejnaluk.gamenewscrawler.data.EntryDb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 @org.springframework.context.annotation.Configuration
 class RedisConfiguration {
@@ -21,17 +27,26 @@ class RedisConfiguration {
     Logger logger = LoggerFactory.getLogger(RedisConfiguration.class.getName());
 
     @Bean
-    public JedisPool getJedisPool() {
+    JedisConnectionFactory jedisConnectionFactory() {
         URI redisURI = URI.create(REDIS_URL);
         logger.info("Redis info " + REDIS_URL);
         logger.info("Redis info " + redisURI.getHost() + ":" + redisURI.getPort());
-        JedisPool jedisPool= new JedisPool(new JedisPoolConfig(),
-                redisURI.getHost(),
-                redisURI.getPort(),
-                Protocol.DEFAULT_TIMEOUT,
-                redisURI.getUserInfo() == null ? null : redisURI.getUserInfo().split(":",2)[1]);
 
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisURI.getHost());
+        redisStandaloneConfiguration.setPort(redisURI.getPort());
+        redisStandaloneConfiguration.setDatabase(0);
+        redisStandaloneConfiguration.setPassword(redisURI.getUserInfo() == null ? null : redisURI.getUserInfo().split(":",2)[1]);
 
-        return jedisPool;
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, EntryDb> redisTemplate() {
+        RedisTemplate<String, EntryDb> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 }
