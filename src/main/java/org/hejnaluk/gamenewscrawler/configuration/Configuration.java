d541217308b641c81ae276948c4020af474ecd30
Lukáš Hejnal <kejhy93@gmail.com>
2021-07-25 14:09:39 +0200

    Use REDIS_URL variable

--- src/main/java/org/hejnaluk/gamenewscrawler/configuration/Configuration.java
+++ src/main/java/org/hejnaluk/gamenewscrawler/configuration/Configuration.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.hejnaluk.gamenewscrawler.service.HttpRequestExecutor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
--- src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
+++ src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
@@ -1,11 +1,10 @@
 package org.hejnaluk.gamenewscrawler.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
@@ -13,26 +12,26 @@ import redis.clients.jedis.Protocol;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
-
 @org.springframework.context.annotation.Configuration
 class RedisConfiguration {
 
     @Value("${REDIS_URL}")
     String REDIS_URL;
 
+    Logger logger = LoggerFactory.getLogger(RedisConfiguration.class.getName());
+
     @Bean
     public JedisPool getJedisPool() {
-        try {
-            URI redisURI = new URI(REDIS_URL);
-            return new JedisPool(new JedisPoolConfig(),
-                    redisURI.getHost(),
-                    redisURI.getPort(),
-                    Protocol.DEFAULT_TIMEOUT,
-                    redisURI.getUserInfo() == null ? null : redisURI.getUserInfo().split(":",2)[1]);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Redis couldn't be configured from URL in REDIS_URL env var:"+
-                    System.getenv("REDIS_URL"));
-        }
+        URI redisURI = URI.create(REDIS_URL);
+        logger.info("Redis info " + REDIS_URL);
+        logger.info("Redis info " + redisURI.getHost() + ":" + redisURI.getPort());
+        JedisPool jedisPool= new JedisPool(new JedisPoolConfig(),
+                redisURI.getHost(),
+                redisURI.getPort(),
+                Protocol.DEFAULT_TIMEOUT,
+                redisURI.getUserInfo() == null ? null : redisURI.getUserInfo().split(":",2)[1]);
+
+
+        return jedisPool;
     }
 }
