c20ca64cbf3f3179ca95bca6bafc090fbbd341da
Lukáš Hejnal <kejhy93@gmail.com>
2021-07-25 13:29:19 +0200

    Try Jedis

--- src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
+++ src/main/java/org/hejnaluk/gamenewscrawler/configuration/RedisConfiguration.java
@@ -6,31 +6,30 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
 
 @org.springframework.context.annotation.Configuration
 class RedisConfiguration {
 
-    @Value("${REDIS_URL}")
-    String REDIS_URL;
-
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .readFrom(REPLICA_PREFERRED)
-                .build();
-
-        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(REDIS_URL);
-
-        return new LettuceConnectionFactory(serverConfig, clientConfig);
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplateBean() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-        return template;
+    public JedisPool getJedisPool() {
+        try {
+            URI redisURI = new URI(System.getenv("REDIS_URL"));
+            return new JedisPool(new JedisPoolConfig(),
+                    redisURI.getHost(),
+                    redisURI.getPort(),
+                    Protocol.DEFAULT_TIMEOUT,
+                    redisURI.getUserInfo().split(":",2)[1]);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Redis couldn't be configured from URL in REDIS_URL env var:"+
+                    System.getenv("REDIS_URL"));
+        }
     }
 }
